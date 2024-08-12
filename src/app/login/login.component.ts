97860ffc80865cb5bd7be77803689c3a2b3bf0b5
jsczzzx <jsczzzx@yahoo.com>
2024-08-12 07:53:06 -0400

    commit-2

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  signupForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.signupForm = this.fb.group({
+      first_name: ['', Validators.required],
+      last_name: ['', Validators.required],
+      mobile_no: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirm_password: ['', Validators.required]
+    });
+  }
+
+  navigateToSignIn() {}
+
+  onSubmit() {
+    if (this.signupForm.valid) {
+      // Use JSON.stringify to convert the form values object into a string for alert
+      alert(JSON.stringify(this.signupForm.value, null, 2));
+    } else {
+      alert('Please fill out the form correctly before submitting.');
+    }
+  }
 
 }
